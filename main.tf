bcd773b1e0b946a0334a6463c33c40fc49f72d4a
ronaldjeden <ronald@sytek.nl>
2021-03-24 18:08:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "nomad-node" {
     vpc_security_group_ids = [aws_security_group.nomad-sg.id]
     associate_public_ip_address = false
     user_data = file("conf/install-nomad.sh")
-    private_ip = "10.0.${count.index}.100"
+    private_ip = "10.0.1${count.index}.100"
 
     tags = {
         Terraform = "true"
@@ -16,48 +16,5 @@ resource "aws_instance" "nomad-node" {
         Name = "nomad-node-${count.index}"
     }
 }
-resource "aws_iam_role_policy" "staging-client-docker_ecr_policy" {
-  name = "test_policy"
-  role = aws_iam_role.test_role.id
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-                "ecr:GetAuthorizationToken",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:GetRepositoryPolicy",
-                "ecr:DescribeRepositories",
-                "ecr:ListImages",
-                "ecr:DescribeImages",
-                "ecr:BatchGetImage"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
 
