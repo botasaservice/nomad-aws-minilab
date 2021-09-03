3c41d75cb0a190de6136a16c9e788a73462035af
ronaldjeden <ronald@sytek.nl>
2021-09-03 12:31:42 +0200

    Update install-nomad.sh

--- conf/install-nomad.sh
+++ conf/install-nomad.sh
@@ -18,7 +18,7 @@ fi
 
 # Install docker
 sudo yum install -y git
-sudo amazon-linux-extras install docker
+sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chkconfig docker on
